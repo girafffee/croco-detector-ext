8403a07a0b4388c64d92aa834940be9c9ecd9cde
hug.reed <hug.reed@N4600.TM>
2020-08-05 16:10:30 +0300

    Fix ajax request without async

--- js/detector.js
+++ js/detector.js
@@ -1,18 +1,18 @@
 // Inform the background page that 
 // this tab should have a page-action.
 function get_wp_json(ajax_url) { 
+    
     let namespaces;
-    return new Promise( resolve => {
-        $.ajax({
-            url: ajax_url,
-            async: true,
-            success: function( data ) {	
-                console.log( data.namespaces );
-                namespaces = data.namespaces;
-            }
-        });
-        resolve(namespaces);
+    $.ajax({
+        url: ajax_url,
+        async: false,
+        success: function( data ) {	
+            console.log( data.namespaces );
+            namespaces = data.namespaces;
+        }
     });
+    return namespaces;
+
 }
 
 chrome.runtime.sendMessage({
@@ -21,7 +21,7 @@ chrome.runtime.sendMessage({
   });
   
   // Listen for messages from the popup.
-  chrome.runtime.onMessage.addListener(async (msg, sender, response) => {
+  chrome.runtime.onMessage.addListener( (msg, sender, response) => {
     // First, validate the message's structure.
     if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
       // Collect the necessary data. 
@@ -33,18 +33,14 @@ chrome.runtime.sendMessage({
             buttons: document.querySelectorAll('button').length,
             json_api_url: document.querySelector('link[rel="https://api.w.org/"]').href
         };
-        await saveNamespaces(domInfo);
-        
-  
+        domInfo.namespaces = get_wp_json(domInfo.json_api_url);
+
       // Directly respond to the sender (popup), 
       // through the specified callback.
         response(domInfo);
     }
   });
 
-async function saveNamespaces(object) {
-    object.namespaces = await get_wp_json(object.json_api_url);
-}
 
 
 
