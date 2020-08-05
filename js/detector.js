21f3d2a824285dd6fd30072085ca0ab1555a07a0
hug.reed <hug.reed@N4600.TM>
2020-08-05 15:17:24 +0300

    Add ajax request to wp-json (not async)

--- js/detector.js
+++ js/detector.js
@@ -1,9 +1,54 @@
-(function () {
-    let wp_ver = document.querySelector('meta[name="generator"]');
-    if ( ! wp_ver ) {
-        return false;
+// Inform the background page that 
+// this tab should have a page-action.
+function get_wp_json(ajax_url) { 
+    let namespaces;
+    return new Promise( resolve => {
+        $.ajax({
+            url: ajax_url,
+            async: true,
+            success: function( data ) {	
+                console.log( data.namespaces );
+                namespaces = data.namespaces;
+            }
+        });
+        resolve(namespaces);
+    });
+}
+
+chrome.runtime.sendMessage({
+    from: 'content',
+    subject: 'showPageAction',
+  });
+  
+  // Listen for messages from the popup.
+  chrome.runtime.onMessage.addListener(async (msg, sender, response) => {
+    // First, validate the message's structure.
+    if ((msg.from === 'popup') && (msg.subject === 'DOMInfo')) {
+      // Collect the necessary data. 
+      // (For your specific requirements `document.querySelectorAll(...)`
+      //  should be equivalent to jquery's `$(...)`.)
+        var domInfo = {
+            total: document.querySelectorAll('*').length,
+            inputs: document.querySelectorAll('input').length,
+            buttons: document.querySelectorAll('button').length,
+            json_api_url: document.querySelector('link[rel="https://api.w.org/"]').href
+        };
+        await saveNamespaces(domInfo);
+        
+  
+      // Directly respond to the sender (popup), 
+      // through the specified callback.
+        response(domInfo);
     }
-    console.log(chrome.extension.getBackgroundPage());
-})()
+  });
+
+async function saveNamespaces(object) {
+    object.namespaces = await get_wp_json(object.json_api_url);
+}
+
+
+
+
+
 
 
